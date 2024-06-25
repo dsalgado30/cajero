5e97c4940b7f85fb2501e21f0a0340f51cb10435
daniela-sc <daniela148sc@gmail.com>
2024-06-25 18:33:45 -0500

    cambios

---
+++ cajero.php
@@ -0,0 +1,12 @@
+<?php
+do {
+    echo "---opciones---";
+    echo "1. Consultar saldo ";
+    echo "2. Consignar dinero ";
+    echo "3. Retirar dinero ";
+    echo "4. Salir";
+
+    $opcion= readline ("Seleccione una opcion: ");
+
+    if ($opcion=="1")
+}
